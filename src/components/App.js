e99d750172658e27990c0f10b883e9e3dcc0d434
Alexx2210 <alexxandr008@gmail.com>
2023-01-28 17:41:06 +0300

    Added search + gh-pages (package.json)

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,16 @@ import '../styles/App.css';
 function App() {
 
     const API_URL="https://api.themoviedb.org/3/movie/popular?api_key=ead762915fa943d470035bcf89cf913e";
+    const API_SEARCH="https://api.themoviedb.org/3/search/movie?api_key=ead762915fa943d470035bcf89cf913e&query=";
     const [movies, setMovies] = useState([]);
+    const [term, setTerm] = useState([]);
+    const handleSearch = (event) => {
+        event.preventDefault();
+
+        fetch(API_SEARCH + term)
+        .then(res => res.json())
+        .then(data => setMovies(data.results))
+    }
 
     useEffect(() => {
         fetch(API_URL)
@@ -24,6 +33,10 @@ function App() {
             <main>
                 <header>
                     <h1>Movie Search</h1>
+                    <form onSubmit={handleSearch}>
+                        <input onChange={(event) => setTerm(event.target.value) }/>
+                        <button>Search</button>
+                    </form>
                 </header>
             <div className="container">
                 {movies.map((movieReq) => 
